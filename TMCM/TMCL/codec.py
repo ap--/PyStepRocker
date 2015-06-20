7152fc05754719c06fbf50fa416ac91f42b3ee3c
Nicht Jens <nichtjens@gmail.com>
2015-06-20 20:12:11 +0200

    refactored conversion to byte into function

--- TMCM/TMCL/codec.py
+++ TMCM/TMCL/codec.py
@@ -2,15 +2,20 @@
 from error import *
 
 
+def byte(n):
+    """Convert n to byte in range(256)"""
+    return int(n) % (1<<8)
+
+
 def encodeRequestCommand(m_address, n_command, n_type, n_motor, value, debug=False):
     # convert to bytes
-    m_address = int(m_address) % (1<<8)
-    n_command = int(n_command) % (1<<8)
-    n_type = int(n_type) % (1<<8)
-    n_motor = int(n_motor) % (1<<8)
-    value = [(int(value) >> i*8) % (1<<8) for i in range(3,-1,-1)]
+    m_address = byte(m_address)
+    n_command = byte(n_command)
+    n_type = byte(n_type)
+    n_motor = byte(n_motor)
+    value = [byte(int(value) >> i*8) for i in range(3,-1,-1)]
     # generate command
-    checksum = (m_address + n_command + n_type + n_motor + sum(value)) % (1<<8)
+    checksum = byte(m_address + n_command + n_type + n_motor + sum(value))
     tmcl_bytes = [m_address, n_command, n_type, n_motor] + value + [checksum]
     tmcl_cmd = sum(b << (8-i)*8 for i,b in enumerate(tmcl_bytes))
     if debug:
@@ -19,13 +24,13 @@ def encodeRequestCommand(m_address, n_command, n_type, n_motor, value, debug=Fal
 
 def encodeReplyCommand(r_address, m_address, status, n_command, value, debug=False):
     # convert to bytes
-    r_address = int(r_address) % (1<<8)
-    m_address = int(m_address) % (1<<8)
-    status = int(status) % (1<<8)
-    n_command = int(n_command) % (1<<8)
-    value = [(int(value) >> i*8) % (1<<8) for i in range(3,-1,-1)]
+    r_address = byte(r_address)
+    m_address = byte(m_address)
+    status = byte(status)
+    n_command = byte(n_command)
+    value = [byte(int(value) >> i*8) for i in range(3,-1,-1)]
     # generate command
-    checksum = (r_address + m_address + status + n_command + sum(value)) % (1<<8)
+    checksum = byte(r_address + m_address + status + n_command + sum(value))
     tmcl_bytes = [r_address, m_address, status, n_command] + value + [checksum]
     tmcl_cmd = sum(b << (8-i)*8 for i,b in enumerate(tmcl_bytes))
     if debug:
