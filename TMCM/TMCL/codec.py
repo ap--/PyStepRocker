e00ad335d5b3a78b6aa728bdf58859f68c4a5af9
NichtJens <nichtjens@gmail.com>
2015-06-26 16:53:25 +0200

    refactored codec for better code re-use and readability

--- TMCM/TMCL/codec.py
+++ TMCM/TMCL/codec.py
@@ -2,69 +2,77 @@
 from error import *
 
 
+COMMAND_STRING_LENGTH = 9
+
+
 def byte(n):
     """Convert n to byte in range(256)"""
     return int(n) % (1<<8)
 
 
+def checksum(bytes):
+    return byte(sum(bytes))
+
+
+def encodeBytes(value, max_i=3):
+    return [byte(int(value) >> i*8) for i in range(max_i, -1, -1)]
+
+def decodeBytes(bytes, max_i=3):
+    return sum(b << (max_i-i)*8 for i, b in enumerate(bytes))
+
+
+
 def encodeRequestCommand(m_address, n_command, n_type, n_motor, value, debug=False):
-    # convert to bytes
-    m_address = byte(m_address)
-    n_command = byte(n_command)
-    n_type = byte(n_type)
-    n_motor = byte(n_motor)
-    value = [byte(int(value) >> i*8) for i in range(3,-1,-1)]
-    # generate command
-    checksum = byte(m_address + n_command + n_type + n_motor + sum(value))
-    tmcl_bytes = [m_address, n_command, n_type, n_motor] + value + [checksum]
-    tmcl_cmd = sum(b << (8-i)*8 for i,b in enumerate(tmcl_bytes))
-    if debug:
-        print "{0:0>18X}".format(tmcl_cmd), "".join([chr(b) for b in tmcl_bytes])
-    return "".join([chr(b) for b in tmcl_bytes])
+    return encodeCommand([m_address, n_command, n_type, n_motor], value, debug)
 
 def encodeReplyCommand(r_address, m_address, status, n_command, value, debug=False):
-    # convert to bytes
-    r_address = byte(r_address)
-    m_address = byte(m_address)
-    status = byte(status)
-    n_command = byte(n_command)
-    value = [byte(int(value) >> i*8) for i in range(3,-1,-1)]
-    # generate command
-    checksum = byte(r_address + m_address + status + n_command + sum(value))
-    tmcl_bytes = [r_address, m_address, status, n_command] + value + [checksum]
-    tmcl_cmd = sum(b << (8-i)*8 for i,b in enumerate(tmcl_bytes))
+    return encodeCommand([r_address, m_address, status, n_command], value, debug)
+
+
+def encodeCommand(parameters, value, debug):
+    bytes = map(byte, parameters)
+    value = encodeBytes(value)
+    bytes += value
+    chsum = checksum(bytes)
+    bytes += [chsum]
+    result = "".join([chr(b) for b in bytes])
+
     if debug:
-        print "{0:0>18X}".format(tmcl_cmd), "".join([chr(b) for b in tmcl_bytes])
-    return "".join([chr(b) for b in tmcl_bytes])
+        cmd = decodeBytes(bytes, max_i=8)
+        print "{0:0>18X}".format(cmd), result
+    return result
+
+
 
 def decodeRequestCommand(cmd_string):
-    byte_array = bytearray(cmd_string)
-    if len(byte_array) != 9:
-        raise TMCLError("Commandstring shorter than 9 bytes")
-    if byte_array[8] != byte(sum(byte_array[:8])):
-        raise TMCLError("Checksum error in command %s" % cmd_string)
+    byte_array = decodeCommand(cmd_string)
     ret = {}
     ret['module-address'] = byte_array[0]
     ret['command-number'] = byte_array[1]
-    ret['type-number'] = byte_array[2]
-    ret['motor-number'] = byte_array[3]
-    ret['value'] = sum(b << (3-i)*8 for i,b in enumerate(byte_array[4:8]))
-    ret['checksum'] = byte_array[8]
-    return ret    
+    ret['type-number']    = byte_array[2]
+    ret['motor-number']   = byte_array[3]
+    ret['value']          = decodeBytes(byte_array[4:8])
+    ret['checksum']       = byte_array[8]
+    return ret
 
 def decodeReplyCommand(cmd_string):
-    byte_array = bytearray(cmd_string)
-    if len(byte_array) != 9:
-        raise TMCLError("Commandstring shorter than 9 bytes")
-    if byte_array[8] != byte(sum(byte_array[:8])):
-        raise TMCLError("Checksum error in command %s" % cmd_string)
+    byte_array = decodeCommand(cmd_string)
     ret = {}
-    ret['reply-address'] = byte_array[0]
+    ret['reply-address']  = byte_array[0]
     ret['module-address'] = byte_array[1]
-    ret['status'] = byte_array[2]
+    ret['status']         = byte_array[2]
     ret['command-number'] = byte_array[3]
-    ret['value'] = sum(b << (3-i)*8 for i,b in enumerate(byte_array[4:8]))
-    ret['checksum'] = byte_array[8]
+    ret['value']          = decodeBytes(byte_array[4:8])
+    ret['checksum']       = byte_array[8]
     return ret
+
+
+def decodeCommand(cmd_string):
+    byte_array = bytearray(cmd_string)
+    if len(byte_array) != COMMAND_STRING_LENGTH:
+        raise TMCLError("Commandstring shorter than {} bytes".format(COMMAND_STRING_LENGTH))
+    if byte_array[8] != checksum(byte_array[:8]):
+        raise TMCLError("Checksum error in command {}".format(cmd_string))
+    return byte_array
 
 
