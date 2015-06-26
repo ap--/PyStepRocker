e105bb3ca4748d95ed12a5283d851e9c3f6e8e9d
NichtJens <nichtjens@gmail.com>
2015-06-26 20:53:49 +0200

    added docstrings where there was none so far

--- TMCM/TMCL/codec.py
+++ TMCM/TMCL/codec.py
@@ -11,25 +11,36 @@ def byte(n):
 
 
 def checksum(bytes):
+    """Calculate checksum byte for given list of bytes"""
     return byte(sum(bytes))
 
 
 def encodeBytes(value, max_i=3):
+    """Encode a value to a byte list"""
     return [byte(int(value) >> i*8) for i in range(max_i, -1, -1)]
 
 def decodeBytes(bytes, max_i=3):
+    """Decode a byte list to a value"""
     return sum(b << (max_i-i)*8 for i, b in enumerate(bytes))
 
 
 
 def encodeRequestCommand(m_address, n_command, n_type, n_motor, value, debug=False):
+    """Encode a request using encodeCommand"""
     return encodeCommand([m_address, n_command, n_type, n_motor], value, debug)
 
 def encodeReplyCommand(r_address, m_address, status, n_command, value, debug=False):
+    """Encode a reply using encodeCommand"""
     return encodeCommand([r_address, m_address, status, n_command], value, debug)
 
 
 def encodeCommand(parameters, value, debug):
+    """
+    Encode a command string:
+    Convert parameters and value to byte lists
+    Calculate the checksum
+    Join everything and convert to string
+    """
     bytes = map(byte, parameters)
     value = encodeBytes(value)
     bytes += value
@@ -45,6 +56,7 @@ def encodeCommand(parameters, value, debug):
 
 
 def decodeRequestCommand(cmd_string):
+    """Decode a request using decodeCommand, fill dict with result"""
     byte_array = decodeCommand(cmd_string)
     ret = {}
     ret['module-address'] = byte_array[0]
@@ -56,6 +68,7 @@ def decodeRequestCommand(cmd_string):
     return ret
 
 def decodeReplyCommand(cmd_string):
+    """Decode a reply using decodeCommand, fill dict with result"""
     byte_array = decodeCommand(cmd_string)
     ret = {}
     ret['reply-address']  = byte_array[0]
@@ -68,6 +81,7 @@ def decodeReplyCommand(cmd_string):
 
 
 def decodeCommand(cmd_string):
+    """Convert command string to byte list with some checks"""
     byte_array = bytearray(cmd_string)
     if len(byte_array) != COMMAND_STRING_LENGTH:
         raise TMCLError("Commandstring shorter than {} bytes".format(COMMAND_STRING_LENGTH))
